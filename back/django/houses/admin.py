840127026c28e3db9ba8610274a35d9e9a292d4a
HyunTaek5 <oht222@naver.com>
2021-03-13 20:03:58 +0900

    [update][back] houses model updated

--- back/django/houses/admin.py
+++ back/django/houses/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+
+
+@admin.register(models.House)
+class HouseAdmin(admin.ModelAdmin):
+
+    list_display = ("name", "city", "address", "price",)
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
--- back/django/houses/models.py
+++ back/django/houses/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Photo(core_models.TimeStampedModel):
+
+    caption = models.CharField(max_length=80)
+
+    file = models.ImageField(upload_to="house_photos")
+    house = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.caption
+
+
+class House(core_models.TimeStampedModel):
+    name = models.CharField(max_length=130)
+    description = models.TextField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    amenities = models.CharField(max_length=30)
+    facilities = models.CharField(max_length=30)
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    host = models.CharField(max_length=80)
+    photo = models.ForeignKey("Photo", related_name="photo", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.city = str.capitalize(self.city)
+        super().save(*args, **kwargs)
