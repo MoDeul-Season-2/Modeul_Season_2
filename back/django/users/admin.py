3c55041001ec0f2f67de6fc9a5e13246d1269c94
HyunTaek5 <oht222@naver.com>
2021-03-15 22:48:11 +0900

    [update][back] UserAdmin updated

--- back/django/users/admin.py
+++ back/django/users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("User Detail", {
+            "fields": (
+                "avatar",
+                "gender",
+                "bio",
+                "birthday",
+                "age",
+                "status",
+            ),
+        }),
+    )
+
+    list_filter = UserAdmin.list_filter + ('status', 'gender',)
+
+    list_display = ('username',
+                    'email',
+                    'first_name',
+                    'last_name',
+                    'gender',
+                    'age',
+                    'status',
+                    )
