d0a41a2a61023a0f2df6429ffbebc11f2fae1a05
HyunTaek5 <oht222@naver.com>
2021-03-15 21:08:03 +0900

    [update][back] houses model updated(Amenity, Facility, AbstractItem added on House)

--- back/django/houses/models.py
+++ back/django/houses/models.py
@@ -2,6 +2,30 @@ from django.db import models
 from core import models as core_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+
+    name = models.CharField(max_length=80)
+    subtitle = models.TextField(blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Amenity(AbstractItem):
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
 class Photo(core_models.TimeStampedModel):
 
     caption = models.CharField(max_length=80)
@@ -18,8 +42,8 @@ class House(core_models.TimeStampedModel):
     description = models.TextField()
     city = models.CharField(max_length=80)
     price = models.IntegerField()
-    address = models.CharField(max_length=140)
-    amenities = models.CharField(max_length=30)
+    address = models.ManyToManyField(Amenity, blank=True)
+    amenities = models.ManyToManyField(Facility, blank=True)
     facilities = models.CharField(max_length=30)
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
