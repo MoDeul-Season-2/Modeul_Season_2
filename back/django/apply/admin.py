2efe910daed80e94c4543fade0b76945729a2f32
HyunTaek5 <oht222@naver.com>
2021-03-16 23:33:14 +0900

    [update][back] apply admin registered

--- back/django/apply/admin.py
+++ back/django/apply/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Apply)
+class ApplyAdmin(admin.ModelAdmin):
+
+    list_display = ("applyName", "house", "host", "guest", "price",)
