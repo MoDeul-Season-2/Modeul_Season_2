48acb723f2d4c06a73bda5ec608934b9576553c4
HyunTaek5 <oht222@naver.com>
2021-03-16 23:21:42 +0900

    [update][back] apply models added (applyName, house, host, guest, price)

--- back/django/apply/models.py
+++ back/django/apply/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Apply(core_models.TimeStampedModel):
+    applyName = models.CharField(max_length=50)
+    house = models.CharField(max_length=100)
+    host = models.CharField(max_length=50)
+    guest = models.CharField(max_length=50)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.house
