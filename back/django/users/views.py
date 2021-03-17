417e482216eefcfab42d9b2ec120de4a29caf1af
HyunTaek5 <oht222@naver.com>
2021-03-17 22:32:12 +0900

    [update][back] UserView restframework added

--- back/django/users/views.py
+++ back/django/users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from  rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
+from .models import User
 
-# Create your views here.
+
+class UserView(APIView):
+
+    def post(self, request):
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, **kwargs):
+        if kwargs.get('id') is None:
+            return Response("invalid Request", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            id = kwargs.get('id')
+            user_object = User.objects.get(id=id)
+
+            update_user_serializer = UserSerializer(user_object, data=request.data)
+            if update_user_serializer.is_valid():
+                update_user_serializer.save()
+                return Response(update_user_serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response("invalid Request", status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, **kwargs):
+        if kwargs.get('id') is None:
+            return Response('invalid Request', status=status.HTTP_400_BAD_REQUEST)
+        else:
+            id = kwargs.get('id')
+            user_object = User.objects.get(id=id)
+            user_object.delete()
+            return Response("deleted", status=status.HTTP_200_OK)
