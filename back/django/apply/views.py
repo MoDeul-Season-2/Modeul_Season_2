0f2d6451dc4d82301d7f3f90cfdae253387e150e
HyunTaek Oh <oht222@naver.com>
2021-03-20 22:14:14 +0900

    [update][back] ApplyView added(DELETE & PUT & GET method)

--- back/django/apply/views.py
+++ back/django/apply/views.py
@@ -27,3 +27,46 @@ class AppliesView(APIView):
         return Response(serializer)
 
 
+class ApplyView(APIView):
+
+    def get_apply(self, pk):
+        try:
+            apply = Apply.objects.get(pk=pk)
+            return apply
+        except Apply.DoesNotExist:
+            return None
+
+    # GET /apply/{apply_id}
+    def get(self, request, pk):
+        apply = self.get_apply(pk)
+        if apply is not None:
+            serializer = ApplySerializer(apply).data
+            return Response(serializer)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # PUT /apply/{apply_id}
+    def put(self, request, pk):
+        apply = self.get_apply(pk)
+        if apply is not None:
+            if apply.host != request.user or apply.guest != request.user:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            serializer = ApplySerializer(apply, data=request.data, partial=True)
+            if serializer.is_valid():
+                apply = serializer.save()
+                return Response(ApplySerializer(apply).data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # DELETE /apply/{apply_id}
+    def delete(self, request, pk):
+        apply = self.get_apply(pk)
+        if apply is not None:
+            if apply.host != request.user:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            apply.delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
