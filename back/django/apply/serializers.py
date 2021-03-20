bcaf7332b326d1731d726addd4febd2865b3f5bb
HyunTaek Oh <oht222@naver.com>
2021-03-20 19:28:02 +0900

    [upload][back] apply serializers.py added(all added)

---
+++ back/django/apply/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Apply
+
+
+class ApplySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Apply
+        fields = "__all__"
